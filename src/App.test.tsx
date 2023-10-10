3aca5ec405da6e80ab94f73f1306ad8cfb5c6676
Narendran Ethiraj <narendrn@gmail.com>
2023-10-11 10:42:17 +1100

    Unit testing for App and CustomerList

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import App from './App';
-
 describe('Index', () => {
-test('renders App and validates heading', () => {
-  render(<App />);
+test('renders App and validates heading', async () => {
+  await act(() => {
+    render(<App />);
+  }); 
   const textElement = screen.getByText(/user types/i);
   const listElement = screen.getByTestId('customer-lists');
   expect(textElement).toBeInTheDocument();
@@ -11,12 +12,47 @@ test('renders App and validates heading', () => {
 });
 
 test('Should show the Radio buttons', async () => { 
-    render(<App />);  
+    await act(() => {
+      render(<App />);
+    }); 
     const adminRadio = screen.getByLabelText('Admin');
     const managerRadio = screen.getByLabelText('Manager');
     expect(adminRadio).toBeChecked();
-    fireEvent.click(managerRadio);
+    await act(() => {
+      fireEvent.click(managerRadio);
+    });   
     expect(adminRadio).not.toBeChecked();
     expect(managerRadio).toBeChecked();
   });
 });
+/*describe('Mock Fetch', () => {
+  let originFetch: any;
+  beforeEach(() => {
+    originFetch = (global as any).fetch;
+  });
+  afterEach(() => {
+    (global as any).fetch = originFetch;
+  });
+  it('should pass', async () => {
+    const customerData = {
+      data: {
+        listZellerCustomers: {
+          items: [
+            { name: 'John Doe', email:'john@gmail.com', id:'0482', role:'MANAGER' },
+            { name: 'Jane Smith', email:'smith@gmail.com', id:'0482', role:'MANAGER'},
+          ]
+        }
+      }
+    }
+    const fakeResponse = customerData;
+    const mRes = { json: jest.fn().mockResolvedValueOnce(fakeResponse) };
+    const mockedFetch = jest.fn().mockResolvedValueOnce(mRes as any);
+    (global as any).fetch = mockedFetch;
+    await act(() => {
+      render(<App />);
+    });
+    screen.debug();
+    expect(mockedFetch).toBeCalledTimes(1);
+    expect(mRes.json).toBeCalledTimes(1);
+  });
+});*/
--- src/CustomerList.test.tsx
+++ src/CustomerList.test.tsx
@@ -1,14 +1,16 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, act } from "@testing-library/react"
 
 import { CustomerList } from './CustomerList';
 describe('Customer List', () => {
-      it("should render list of customers", () => {
+      it("should render list of customers", async () => {
         const filteredUser = [
           { name: 'John Doe', email:'john@gmail.com', id:'0482', role:'MANAGER' },
           { name: 'Jane Smith', email:'smith@gmail.com', id:'0482', role:'MANAGER'},
         ];
         const userType = 'Admin';
-        render(<CustomerList filteredUser={filteredUser} userType={userType} />);
+        act(() => {
+          render(<CustomerList filteredUser={filteredUser} userType={userType} />);
+        });
         const items = screen.getAllByRole("listitem")
         // screen.debug();
         expect(items.length).toBe(filteredUser.length)
